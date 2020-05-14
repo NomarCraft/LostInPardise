28aa5a757ec77ca5709cf29e2405640bec67e573
Leopold Cramer <2lcramer@gmail.com>
2020-05-14 13:07:40 +0200

    Add : Base InventorySystem

--- Assets/Scripts/Leopold/Item.cs
+++ Assets/Scripts/Leopold/Item.cs
@@ -2,28 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
  
-public class Item {
-    public int id;
-    public string title;
+ [CreateAssetMenu(fileName = "New Item", menuName = "Inventory System/Item")]
+public class Item : ScriptableObject{
+
+    public GameObject prefab;
+    
+    [TextArea(1, 20)]
     public string description;
-    public Sprite icon;
-    public Dictionary<string, int> stats = new Dictionary<string, int>();
- 
-    public Item(int id, string title, string description, Sprite icon, Dictionary<string, int> stats)
-    {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.icon = icon;
-        this.stats = stats;
-    }
- 
-    public Item(Item item)
-    {
-        this.id = item.id;
-        this.title = item.title;
-        this.description = item.description;
-        this.icon = item.icon;
-        this.stats = item.stats;
-    }
+    
 }
