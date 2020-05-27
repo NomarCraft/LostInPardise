90c6f33a64811a6ed67588ca728397af218d122e
NomarCraft <48915344+NomarCraft@users.noreply.github.com>
2020-05-27 12:29:39 +0200

    DialogueSystem

---
+++ Assets/Scripts/Martin/Interactables/Dialoguable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialoguable : Interactable
+{
+	[Space(10)]
+	[Header("Dialogue")]
+	public int _logId;
+	public string _dialogueText;
+
+	public void Interaction(out string text)
+	{
+		text = _dialogueText;
+	}
+}
